83412ee33dd7f49612fca617c0fa11419f42c4c9
Nick Gammon <nick@gammon.com.au>
2012-12-10 20:02:50 +1100

    Added site I got the .hex file from

--- Atmega_Board_Programmer/bootloader_atmega2560_v2.h
+++ Atmega_Board_Programmer/bootloader_atmega2560_v2.h
@@ -1,6 +1,7 @@
 // File =  stk500boot_v2_mega2560_fixes_watchdog_problem.hex
 // Loader start: 3E000, length: 8192
 // MD5 sum = 8A F4 7A 29 43 A0 D8 7C DB ED 09 A3 8F 40 24 1E 
+// https://github.com/arduino/Arduino-stk500v2-bootloader/blob/master/goodHexFiles/stk500boot_v2_mega2560.hex
 
 byte PROGMEM atmega2560_v2 [] = {
 0x0D, 0x94, 0x89, 0xF1, 0x0D, 0x94, 0xB2, 0xF1, 0x0D, 0x94, 0xB2, 0xF1, 0x0D, 0x94, 0xB2, 0xF1, 
